3051cfb6b866a2e51eb0f800123e634c803bf3f7
Ben Phegan <benphegan@gmail.com>
2022-08-23 22:54:13 +1000

    Provide an easy way of outputting an MD5 hash of the rcon password

--- output.tf
+++ output.tf
@@ -8,3 +8,9 @@ output "ec2_global_ip" {
   description = "Public IP address of the server"
   value       = aws_instance.pavlov.public_ip
 }
+
+output "rcon_password_md5_hash" {
+  description = "MD5 hash of the rcon password, ready for use"
+  value       = md5(var.rcon_password)
+  sensitive   = true
+}
